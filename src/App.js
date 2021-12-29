e1d96f505e6ffc31c9ac3dafea7a2c15d9e4ffad
c <mrc3noriega@gmail.com>
2021-12-29 23:17:12 +0100

    Formik and MaterialUI init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import OnboardFlow from './components/OnboardFlow';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const myTheme = createTheme({
+	palette: {
+		mode: 'dark',
+		dark: { primary: '#f02934', divider: '#f02934' },
+	},
+	typography: {
+		fontFamily: 'Roboto Mono',
+		fontSize: 18,
+	},
+});
 
 const App = () => {
 	return (
-		<div>
-			<h1>App</h1>
-		</div>
+		<ThemeProvider theme={myTheme}>
+			<div className="app">
+				<Navbar />
+				<h1>NFT bots that really go BRR 🤑</h1>
+				<OnboardFlow />
+			</div>
+		</ThemeProvider>
 	);
 };
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../img/moonbots-logo.svg';
+import twitterIcon from '../img/twitter-white.svg';
+
+const Navbar = () => {
+	return (
+		<div className="nav">
+			<div className="logo-wrapper">
+				<img className="logo" src={logo} alt="moonbots logo" />
+			</div>
+			<ul className="main-menu">
+				<a href="https://twitter.com/WenMoonBots">
+					<li className="menu-item">
+						<img
+							src={twitterIcon}
+							alt="twitter icon"
+							className="twitter-icon"
+						/>
+					</li>
+				</a>
+			</ul>
+		</div>
+	);
+};
+
+export default Navbar;
