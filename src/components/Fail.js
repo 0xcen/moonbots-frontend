0b8abd7a9db941209af66ff6171d6b9323ec96b2
cenoriega <75177443+cenoriega@users.noreply.github.com>
2022-01-05 14:12:40 +0100

    Update Fail.js

--- src/components/Fail.js
+++ src/components/Fail.js
@@ -5,6 +5,9 @@ const Fail = (props) => {
 	return (
 		<div className="container centered">
 			<h2 className="h2-loud">Oh no! Something went Wrong.</h2>
+			<a className="link" href="/signup">
+				Try Again
+			</a>
 			<p>{JSON.stringify(...props)}</p>
 			<div></div>
 		</div>
